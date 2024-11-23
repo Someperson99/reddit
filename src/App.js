edd2f4c3c4618dfb1c319c454ba77b8b8dcbfa35
Someperson99 <geryjuarez48147@gmail.com>
2024-11-23 13:00:58 -0800

    added header to contain logo and a searchbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchHeader from './components/searchHeader';
 
 function App() {
   return (
     <div className="App">
+      <SearchHeader />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
